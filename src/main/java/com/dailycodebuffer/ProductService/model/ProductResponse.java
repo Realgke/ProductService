29fb5a8d96786082766bfc15841d109dd4da27dd
Realgke <134968722+Realgke@users.noreply.github.com>
2024-07-15 00:40:31 +0300

    payment integrated

--- src/main/java/com/dailycodebuffer/ProductService/model/ProductResponse.java
+++ src/main/java/com/dailycodebuffer/ProductService/model/ProductResponse.java
@@ -1,8 +1,12 @@
 package com.dailycodebuffer.ProductService.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
     private String productName;
     private long productId;
